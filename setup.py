e62ec1d164c28ef5ae523e7a2b11a272f92ca403
Ahmad Wilson <awilson@yelp.com>
2016-02-23 17:41:31 -0800

    added setup.py and requirements.txt files for project dependency configuration

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from setuptools import find_packages
+from setuptools import setup
+
+import teamwork
+
+setup(
+    name='teamwork',
+    version=teamwork.__version__,
+    author='releaseeng@yelp.com',
+    author_email='releaseeng@yelp.com',
+    license='Copyright Yelp 2014',
+    packages=find_packages(),
+    description='Team of Bad@$$ virtual agents at your service !)',
+    long_description="""
+       Team Work makes the Dream Work.
+    """,
+    install_requires=[
+        'argparse==1.2.1',
+        'elk==0.3',
+        'Flask==0.10.1',
+        'makeTorrent==0.14'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bot = teamwork.bot:main'
+        ],
+    }
+)
